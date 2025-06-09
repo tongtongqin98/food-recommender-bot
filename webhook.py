249d17da1bb7a49790700bdbaaa2454640908fd0
tongtongqin98 <qintongtong4@gmail.com>
2025-06-09 14:40:33 +0900

    webhook.py

--- webhook.py
+++ webhook.py
@@ -81,5 +81,7 @@ def webhook():
             "fulfillmentText": "Sorry, something went wrong."
         }), 200
 
+import os
+
 if __name__ == "__main__":
-    app.run(port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
